77e830a14b19ae80d973d9677bb0cdfa636c5d3e
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 10:03:43 -0300

    Cargar y abrir pantallas de cliente

--- CapaDePresentacion/PantallasUsuario/PantallasCliente/ClientePrincipal.cs
+++ CapaDePresentacion/PantallasUsuario/PantallasCliente/ClientePrincipal.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
+using System.Windows.Forms;
 
 namespace CapaDePresentacion.PantallasUsuario
 {
@@ -8,5 +9,44 @@ namespace CapaDePresentacion.PantallasUsuario
         {
             InitializeComponent();
         }
+
+
+        #region "Cargar Formulario"
+
+        public void AbrirFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new Form();
+            Formulario = panel1.Controls.OfType<MiFormulario>().FirstOrDefault();
+            Formulario = CrearFormulario<MiFormulario>(Formulario);
+        }
+
+        public Form CrearFormulario<MiFormulario>(Form Formulario) where MiFormulario : Form, new()
+        {
+            if (Formulario == null)
+                Formulario = CargarFormulario<MiFormulario>();
+            else
+                Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        public Form CargarFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new MiFormulario();
+
+            Formulario.TopLevel = false;
+            Formulario.Dock = DockStyle.Fill;
+
+            panel1.Controls.Add(Formulario);
+            panel1.Tag = Formulario;
+
+            Formulario.Show();
+            Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        #endregion
+
     }
 }
