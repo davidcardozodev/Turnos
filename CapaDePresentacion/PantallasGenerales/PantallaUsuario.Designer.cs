05f7e18cb415bb8ed2152ff928d23311b0316fe3
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 00:49:17 -0300

    Pantalla de usuario agregada

---
+++ CapaDePresentacion/PantallasGenerales/PantallaUsuario.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CapaDePresentacion.PantallasGenerales
+{
+    partial class PantallaUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelForm = new System.Windows.Forms.Panel();
+            this.panelcontainer = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.linkCerrarSesion = new System.Windows.Forms.LinkLabel();
+            this.linkPerfil = new System.Windows.Forms.LinkLabel();
+            this.lblPrimerNombre = new System.Windows.Forms.Label();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panelcontainer.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            this.panelTitulo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelForm
+            // 
+            this.panelForm.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelForm.Location = new System.Drawing.Point(200, 51);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Size = new System.Drawing.Size(1309, 599);
+            this.panelForm.TabIndex = 2;
+            // 
+            // panelcontainer
+            // 
+            this.panelcontainer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelcontainer.Controls.Add(this.panelForm);
+            this.panelcontainer.Controls.Add(this.panelMenu);
+            this.panelcontainer.Controls.Add(this.panelTitulo);
+            this.panelcontainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelcontainer.Location = new System.Drawing.Point(0, 0);
+            this.panelcontainer.MinimumSize = new System.Drawing.Size(650, 650);
+            this.panelcontainer.Name = "panelcontainer";
+            this.panelcontainer.Size = new System.Drawing.Size(1509, 650);
+            this.panelcontainer.TabIndex = 1;
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.Black;
+            this.panelMenu.Controls.Add(this.linkCerrarSesion);
+            this.panelMenu.Controls.Add(this.linkPerfil);
+            this.panelMenu.Controls.Add(this.lblPrimerNombre);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 51);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 599);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // linkCerrarSesion
+            // 
+            this.linkCerrarSesion.AutoSize = true;
+            this.linkCerrarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkCerrarSesion.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.linkCerrarSesion.Location = new System.Drawing.Point(12, 483);
+            this.linkCerrarSesion.Name = "linkCerrarSesion";
+            this.linkCerrarSesion.Size = new System.Drawing.Size(168, 30);
+            this.linkCerrarSesion.TabIndex = 5;
+            this.linkCerrarSesion.TabStop = true;
+            this.linkCerrarSesion.Text = "Cerrar sesion";
+            // 
+            // linkPerfil
+            // 
+            this.linkPerfil.AutoSize = true;
+            this.linkPerfil.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkPerfil.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.linkPerfil.Location = new System.Drawing.Point(12, 181);
+            this.linkPerfil.Name = "linkPerfil";
+            this.linkPerfil.Size = new System.Drawing.Size(104, 30);
+            this.linkPerfil.TabIndex = 0;
+            this.linkPerfil.TabStop = true;
+            this.linkPerfil.Text = "Mi perfil";
+            // 
+            // lblPrimerNombre
+            // 
+            this.lblPrimerNombre.AutoSize = true;
+            this.lblPrimerNombre.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimerNombre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPrimerNombre.Location = new System.Drawing.Point(59, 139);
+            this.lblPrimerNombre.Name = "lblPrimerNombre";
+            this.lblPrimerNombre.Size = new System.Drawing.Size(94, 27);
+            this.lblPrimerNombre.TabIndex = 1;
+            this.lblPrimerNombre.Text = "Usuario";
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.Color.Black;
+            this.panelTitulo.Controls.Add(this.lblTitulo);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(0, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(1509, 51);
+            this.panelTitulo.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Black;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(554, 11);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(115, 37);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Tienda";
+            // 
+            // PantallaUsuario
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1509, 586);
+            this.Controls.Add(this.panelcontainer);
+            this.Name = "PantallaUsuario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Turnapp";
+            this.panelcontainer.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
+            this.panelTitulo.ResumeLayout(false);
+            this.panelTitulo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelForm;
+        private System.Windows.Forms.Panel panelcontainer;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.LinkLabel linkCerrarSesion;
+        private System.Windows.Forms.LinkLabel linkPerfil;
+        private System.Windows.Forms.Label lblPrimerNombre;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ CapaDePresentacion/PantallasGenerales/PantallaUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasGenerales
+{
+    public partial class PantallaUsuario : Form
+    {
+        public PantallaUsuario()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+    }
+}
