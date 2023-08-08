fe381a1ed9b9c7db697bf659a616721638c99c52
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 01:01:49 -0300

    Agregada cadena de conexion

---
+++ CapaDeDatos/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeDatos
+{
+    public class Conexion
+    {
+        private SqlConnection _Conexion = new SqlConnection("Server=(local); DataBase=Turnapp;Integrated Security=true");
+
+        public SqlConnection ConexionAbrir()
+        {
+            if (_Conexion.State == ConnectionState.Closed)
+                _Conexion.Open();
+
+            return _Conexion;
+        }
+
+        public SqlConnection ConexionCerrar()
+        {
+            if (_Conexion.State == ConnectionState.Open)
+                _Conexion.Close();
+
+            return _Conexion;
+        }
+
+    }
+}
