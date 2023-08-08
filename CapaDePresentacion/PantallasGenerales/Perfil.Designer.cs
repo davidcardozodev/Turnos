f514ce826edc298799bbf05d10fee633c233786f
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 01:36:26 -0300

    Agregado pantalla de detalles de perfil

---
+++ CapaDePresentacion/PantallasGenerales/Perfil.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace CapaDePresentacion.PantallaGenerales
+{
+    partial class Perfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tituloProfile = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblUsuarioCategoria = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblSegundoNombre = new System.Windows.Forms.Label();
+            this.lblSegundoNombreCategoria = new System.Windows.Forms.Label();
+            this.lblEmailCategoria = new System.Windows.Forms.Label();
+            this.lblRolCategoria = new System.Windows.Forms.Label();
+            this.lblPrimerNombre = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.lblPrimerNombreCategoria = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tituloProfile
+            // 
+            this.tituloProfile.AutoSize = true;
+            this.tituloProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloProfile.Location = new System.Drawing.Point(38, -62);
+            this.tituloProfile.Name = "tituloProfile";
+            this.tituloProfile.Size = new System.Drawing.Size(149, 37);
+            this.tituloProfile.TabIndex = 13;
+            this.tituloProfile.Text = "Mi Profile";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblUsuario.Location = new System.Drawing.Point(597, 122);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(128, 37);
+            this.lblUsuario.TabIndex = 28;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblUsuarioCategoria
+            // 
+            this.lblUsuarioCategoria.AutoSize = true;
+            this.lblUsuarioCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioCategoria.Location = new System.Drawing.Point(410, 122);
+            this.lblUsuarioCategoria.Name = "lblUsuarioCategoria";
+            this.lblUsuarioCategoria.Size = new System.Drawing.Size(128, 37);
+            this.lblUsuarioCategoria.TabIndex = 27;
+            this.lblUsuarioCategoria.Text = "Usuario";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(530, 48);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(89, 37);
+            this.lblTitulo.TabIndex = 37;
+            this.lblTitulo.Text = "Perfil";
+            // 
+            // lblSegundoNombre
+            // 
+            this.lblSegundoNombre.AutoSize = true;
+            this.lblSegundoNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundoNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblSegundoNombre.Location = new System.Drawing.Point(598, 232);
+            this.lblSegundoNombre.Name = "lblSegundoNombre";
+            this.lblSegundoNombre.Size = new System.Drawing.Size(260, 37);
+            this.lblSegundoNombre.TabIndex = 32;
+            this.lblSegundoNombre.Text = "SegundoNombre";
+            // 
+            // lblSegundoNombreCategoria
+            // 
+            this.lblSegundoNombreCategoria.AutoSize = true;
+            this.lblSegundoNombreCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundoNombreCategoria.Location = new System.Drawing.Point(332, 232);
+            this.lblSegundoNombreCategoria.Name = "lblSegundoNombreCategoria";
+            this.lblSegundoNombreCategoria.Size = new System.Drawing.Size(260, 37);
+            this.lblSegundoNombreCategoria.TabIndex = 31;
+            this.lblSegundoNombreCategoria.Text = "SegundoNombre";
+            // 
+            // lblEmailCategoria
+            // 
+            this.lblEmailCategoria.AutoSize = true;
+            this.lblEmailCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailCategoria.Location = new System.Drawing.Point(485, 294);
+            this.lblEmailCategoria.Name = "lblEmailCategoria";
+            this.lblEmailCategoria.Size = new System.Drawing.Size(97, 37);
+            this.lblEmailCategoria.TabIndex = 33;
+            this.lblEmailCategoria.Text = "Email";
+            // 
+            // lblRolCategoria
+            // 
+            this.lblRolCategoria.AutoSize = true;
+            this.lblRolCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRolCategoria.Location = new System.Drawing.Point(501, 342);
+            this.lblRolCategoria.Name = "lblRolCategoria";
+            this.lblRolCategoria.Size = new System.Drawing.Size(64, 37);
+            this.lblRolCategoria.TabIndex = 36;
+            this.lblRolCategoria.Text = "Rol";
+            // 
+            // lblPrimerNombre
+            // 
+            this.lblPrimerNombre.AutoSize = true;
+            this.lblPrimerNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimerNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblPrimerNombre.Location = new System.Drawing.Point(598, 179);
+            this.lblPrimerNombre.Name = "lblPrimerNombre";
+            this.lblPrimerNombre.Size = new System.Drawing.Size(226, 37);
+            this.lblPrimerNombre.TabIndex = 30;
+            this.lblPrimerNombre.Text = "PrimerNombre";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblEmail.Location = new System.Drawing.Point(597, 294);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(97, 37);
+            this.lblEmail.TabIndex = 34;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRol.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblRol.Location = new System.Drawing.Point(598, 342);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(64, 37);
+            this.lblRol.TabIndex = 35;
+            this.lblRol.Text = "Rol";
+            // 
+            // lblPrimerNombreCategoria
+            // 
+            this.lblPrimerNombreCategoria.AutoSize = true;
+            this.lblPrimerNombreCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimerNombreCategoria.Location = new System.Drawing.Point(366, 179);
+            this.lblPrimerNombreCategoria.Name = "lblPrimerNombreCategoria";
+            this.lblPrimerNombreCategoria.Size = new System.Drawing.Size(226, 37);
+            this.lblPrimerNombreCategoria.TabIndex = 29;
+            this.lblPrimerNombreCategoria.Text = "PrimerNombre";
+            // 
+            // PerfilUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 579);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblUsuarioCategoria);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblSegundoNombre);
+            this.Controls.Add(this.lblSegundoNombreCategoria);
+            this.Controls.Add(this.lblEmailCategoria);
+            this.Controls.Add(this.lblRolCategoria);
+            this.Controls.Add(this.lblPrimerNombre);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblRol);
+            this.Controls.Add(this.lblPrimerNombreCategoria);
+            this.Controls.Add(this.tituloProfile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PerfilUsuario";
+            this.Text = "UserProfile";
+           // this.Load += new System.EventHandler(this.UserProfile_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label tituloProfile;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblUsuarioCategoria;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblSegundoNombre;
+        private System.Windows.Forms.Label lblSegundoNombreCategoria;
+        private System.Windows.Forms.Label lblEmailCategoria;
+        private System.Windows.Forms.Label lblRolCategoria;
+        private System.Windows.Forms.Label lblPrimerNombre;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblRol;
+        private System.Windows.Forms.Label lblPrimerNombreCategoria;
+    }
+}
---
+++ CapaDePresentacion/PantallasGenerales/Perfil.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallaGenerales
+{
+    public partial class Perfil : Form
+    {
+        public Perfil()
+        {
+            InitializeComponent();
+        }
+    }
+}
