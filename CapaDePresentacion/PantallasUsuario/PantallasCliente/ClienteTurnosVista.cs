860faa3f979eeaeafc575b17dcc5548db3080083
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 10:02:05 -0300

    Pasar datos de turno extraidos de la base de datos

--- CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnosVista.cs
+++ CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnosVista.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using CapaComun;
+using CapaDeEntidades;
+using CapaDePresentacion.PantallasUsuario.PantallasCliente.Componentes;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace CapaDePresentacion.PantallasUsuario.PantallasCliente
 {
@@ -11,7 +15,28 @@ namespace CapaDePresentacion.PantallasUsuario.PantallasCliente
 
         private void ClienteTurnosVista_Load(object sender, System.EventArgs e)
         {
+            CargarTurnoItem();
         }
+        private void CargarTurnoItem()
+        {
+            Cliente cliente = new Cliente();
 
+            List<FormatoTurnos> ListaTurnos = new List<FormatoTurnos>();
+
+            ListaTurnos = cliente.CargarTurno();
+
+            foreach (FormatoTurnos Turno in ListaTurnos)
+            {
+                ClienteTurnoItem clienteTurnoItem = new ClienteTurnoItem();
+
+                clienteTurnoItem.DiaNombre = Turno.DiaNombre;
+                clienteTurnoItem.DiaNumero = Turno.DiaNumero;
+                clienteTurnoItem.Hora = Turno.Hora;
+                clienteTurnoItem.Descripcion = Turno.Descripcion;
+                clienteTurnoItem.Estado = Turno.Estado;
+
+                flowLayoutPanel1.Controls.Add(clienteTurnoItem);
+            }
+        }
     }
 }
