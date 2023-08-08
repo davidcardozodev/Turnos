71648ec7fc14dccb673b0d7825c21269bd902b25
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 00:21:49 -0300

    Agregado formulario de inicio

---
+++ CapaDePresentacion/PantallasGenerales/Inicio.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PresentationLayer.GeneralScreens
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos Administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede Edit
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSide = new System.Windows.Forms.Panel();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.panelSide.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSide
+            // 
+            this.panelSide.BackColor = System.Drawing.Color.Black;
+            this.panelSide.Controls.Add(this.lblLogin);
+            this.panelSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSide.Location = new System.Drawing.Point(0, 0);
+            this.panelSide.Name = "panelSide";
+            this.panelSide.Size = new System.Drawing.Size(200, 450);
+            this.panelSide.TabIndex = 0;
+            this.panelSide.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoverVentana_MouseDown);
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.BackColor = System.Drawing.Color.Black;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.Color.White;
+            this.lblLogin.Location = new System.Drawing.Point(42, 210);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(120, 32);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Turnapp";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Nirmala UI Semilight", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUsuario.Location = new System.Drawing.Point(283, 114);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(423, 42);
+            this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.Text = "Usuario";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(372, 31);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(210, 32);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "Inicio de sesion";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Font = new System.Drawing.Font("Nirmala UI Semilight", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClave.ForeColor = System.Drawing.Color.DimGray;
+            this.txtClave.Location = new System.Drawing.Point(283, 204);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(423, 42);
+            this.txtClave.TabIndex = 3;
+            this.txtClave.Text = "Clave";
+            this.txtClave.Enter += new System.EventHandler(this.txtClave_Enter);
+            this.txtClave.Leave += new System.EventHandler(this.txtClave_Leave);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Black;
+            this.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(283, 339);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(423, 57);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.botonAcceder_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(278, 281);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(54, 25);
+            this.lblError.TabIndex = 5;
+            this.lblError.Text = "Error";
+            this.lblError.Visible = false;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.panelSide);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tienda";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoverVentana_MouseDown);
+            this.panelSide.ResumeLayout(false);
+            this.panelSide.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSide;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblLogin;
+    }
+}
+
---
+++ CapaDePresentacion/PantallasGenerales/Inicio.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace PresentationLayer.GeneralScreens
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
