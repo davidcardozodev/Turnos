122325488b07f737c86f6e1da44317b841af2ed4
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 01:50:06 -0300

    Metodos de abrir y cargar formulario

--- CapaDePresentacion/PantallasGenerales/PantallaGeneralUsuario.cs
+++ CapaDePresentacion/PantallasGenerales/PantallaGeneralUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
+using System.Windows.Forms;
 
 namespace CapaDePresentacion.PantallasGenerales
 {
@@ -9,5 +10,42 @@ namespace CapaDePresentacion.PantallasGenerales
             InitializeComponent();
             WindowState = FormWindowState.Maximized;
         }
+
+        #region "Cargar Formulario"
+
+        private void AbrirFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new Form();
+            Formulario = panelForm.Controls.OfType<MiFormulario>().FirstOrDefault();
+            Formulario = CrearFormulario<MiFormulario>(Formulario);
+        }
+
+        private Form CrearFormulario<MiFormulario>(Form Formulario) where MiFormulario : Form, new()
+        {
+            if (Formulario == null)
+                Formulario = CargarFormulario<MiFormulario>();
+            else
+                Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        private Form CargarFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new MiFormulario();
+
+            Formulario.TopLevel = false;
+            Formulario.Dock = DockStyle.Fill;
+
+            panelForm.Controls.Add(Formulario);
+            panelForm.Tag = Formulario;
+
+            Formulario.Show();
+            Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        #endregion
     }
 }
