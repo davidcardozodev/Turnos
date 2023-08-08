a4fb6bf8d4977f5a9153f492327beb1f16bf1655
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 00:23:45 -0300

    Agregado mover ventana

--- CapaDePresentacion/PantallasGenerales/Inicio.cs
+++ CapaDePresentacion/PantallasGenerales/Inicio.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace PresentationLayer.GeneralScreens
 {
@@ -8,6 +9,20 @@ namespace PresentationLayer.GeneralScreens
         {
             InitializeComponent();
         }
+
+        #region "Mover ventana"
+        private void MoverVentana_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        [DllImport("Usuario32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("Usuario32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        #endregion
 
     }
 }
