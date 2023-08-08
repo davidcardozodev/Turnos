6783c9b31461999acd0b6a11d2a8b50a7e6a863f
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 01:01:15 -0300

    Agregado entidad usuario

---
+++ CapaDeEntidades/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using CapaComun;
+using CapaDeDatos;
+
+namespace CapaDeEntidades
+{
+    public class Usuario
+    {
+        private ConsultasUsuario _ConsultasUsuario = new ConsultasUsuario();
+
+        public Usuario() { }
+
+        #region "Sesion"
+
+        public bool IniciarSesion(string Usuario, string Clave)
+        {
+            return _ConsultasUsuario.IniciarSesion(Usuario, Clave);
+        }
+
+        public void CerrarSesion()
+        {
+            DatosUsuario.Id = 0;
+            DatosUsuario.Usuario = "";
+            DatosUsuario.Clave = "";
+            DatosUsuario.PrimerNombre = "";
+            DatosUsuario.Email = "";
+            DatosUsuario.Rol = "";
+        }
+
+        #endregion
+    }
+}
