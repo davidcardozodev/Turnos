e8f4103dff3ef66a8f35f7a722f01b2e948e4102
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 09:46:00 -0300

    Agregado formato de los turnos

---
+++ CapaComun/FormatoTurnos.cs
@@ -0,0 +1,12 @@
+﻿namespace CapaComun
+{
+    public class FormatoTurnos
+    {
+        public int Id { get; set; }
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+}
