35d1b91774c927c81f5188762f5f5048bfd68b7b
David Cardozo <davidcardozoemma@gmail.com>
2023-08-08 09:46:46 -0300

    Cliente agregado

---
+++ CapaDeEntidades/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using CapaComun;
+using CapaDeDatos;
+using System.Collections.Generic;
+
+namespace CapaDeEntidades
+{
+    public class Cliente
+    {
+        public Cliente() { }
+
+        private ConsultasUsuario _ConsultasUsuario = new ConsultasUsuario();
+
+        public List<FormatoTurnos> CargarTurno()
+        {
+            return _ConsultasUsuario.CargarTurno();
+        }
+    }
+}
