a37377e3eb3d15a2f8f33cf870969c709bc81a45
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 05:49:07 -0300

    Agregado entidad admin con metodo de cargar turnos

---
+++ CapaDeEntidades/Admin.cs
@@ -0,0 +1,18 @@
+﻿using CapaComun;
+using CapaDeDatos;
+using System.Collections.Generic;
+
+namespace CapaDeEntidades
+{
+    public class Admin
+    {
+        public Admin() { }
+
+        ConsultasAdmin _ConsultasAdmin = new ConsultasAdmin();
+
+        public List<FormatoTurnos> TurnoCargarAdmin()
+        {
+            return _ConsultasAdmin.TurnoCargarAdmin();
+        }
+    }
+}
