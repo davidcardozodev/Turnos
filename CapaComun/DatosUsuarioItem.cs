449ef8c65c52f81e3add3f3e5c334004b9a06fd1
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 07:14:59 -0300

    Agregado datos de usuario (lista)

---
+++ CapaComun/DatosUsuarioItem.cs
@@ -0,0 +1,13 @@
+﻿namespace CapaComun
+{
+    public class DatosUsuarioItem
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string Email { get; set; }
+        public string Rol { get; set; }
+    }
+}
