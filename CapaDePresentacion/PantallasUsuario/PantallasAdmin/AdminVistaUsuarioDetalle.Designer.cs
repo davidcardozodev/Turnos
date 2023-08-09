615a452f2c9a733029e6d3e79b250363e13151cc
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 10:31:26 -0300

    Cargar los datos del usuario en la vista detalle del admin

---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/AdminVistaUsuarioDetalle.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin
+{
+    partial class AdminVistaUsuarioDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblUsuarioCategoria = new System.Windows.Forms.Label();
+            this.lblFechaCategoria = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblEmailCategoria = new System.Windows.Forms.Label();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.lblRolCategoria = new System.Windows.Forms.Label();
+            this.comboRol = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(259, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(266, 37);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Detalle del cliente";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblUsuario);
+            this.panel1.Controls.Add(this.lblUsuarioCategoria);
+            this.panel1.Controls.Add(this.lblFechaCategoria);
+            this.panel1.Controls.Add(this.lblEmail);
+            this.panel1.Controls.Add(this.lblEmailCategoria);
+            this.panel1.Controls.Add(this.lblRol);
+            this.panel1.Controls.Add(this.lblRolCategoria);
+            this.panel1.Location = new System.Drawing.Point(35, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(730, 197);
+            this.panel1.TabIndex = 21;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.lblUsuario.Location = new System.Drawing.Point(168, 24);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(112, 32);
+            this.lblUsuario.TabIndex = 16;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblUsuarioCategoria
+            // 
+            this.lblUsuarioCategoria.AutoSize = true;
+            this.lblUsuarioCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioCategoria.Location = new System.Drawing.Point(37, 24);
+            this.lblUsuarioCategoria.Name = "lblUsuarioCategoria";
+            this.lblUsuarioCategoria.Size = new System.Drawing.Size(112, 32);
+            this.lblUsuarioCategoria.TabIndex = 15;
+            this.lblUsuarioCategoria.Text = "Usuario";
+            // 
+            // lblFechaCategoria
+            // 
+            this.lblFechaCategoria.AutoSize = true;
+            this.lblFechaCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaCategoria.Location = new System.Drawing.Point(94, 24);
+            this.lblFechaCategoria.Name = "lblFechaCategoria";
+            this.lblFechaCategoria.Size = new System.Drawing.Size(0, 32);
+            this.lblFechaCategoria.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.DimGray;
+            this.lblEmail.Location = new System.Drawing.Point(168, 80);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(86, 32);
+            this.lblEmail.TabIndex = 13;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblEmailCategoria
+            // 
+            this.lblEmailCategoria.AutoSize = true;
+            this.lblEmailCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailCategoria.Location = new System.Drawing.Point(56, 80);
+            this.lblEmailCategoria.Name = "lblEmailCategoria";
+            this.lblEmailCategoria.Size = new System.Drawing.Size(86, 32);
+            this.lblEmailCategoria.TabIndex = 9;
+            this.lblEmailCategoria.Text = "Email";
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRol.ForeColor = System.Drawing.Color.DimGray;
+            this.lblRol.Location = new System.Drawing.Point(168, 138);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(57, 32);
+            this.lblRol.TabIndex = 11;
+            this.lblRol.Text = "Rol";
+            // 
+            // lblRolCategoria
+            // 
+            this.lblRolCategoria.AutoSize = true;
+            this.lblRolCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRolCategoria.Location = new System.Drawing.Point(85, 138);
+            this.lblRolCategoria.Name = "lblRolCategoria";
+            this.lblRolCategoria.Size = new System.Drawing.Size(57, 32);
+            this.lblRolCategoria.TabIndex = 7;
+            this.lblRolCategoria.Text = "Rol";
+            // 
+            // comboRol
+            // 
+            this.comboRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboRol.FormattingEnabled = true;
+            this.comboRol.Location = new System.Drawing.Point(229, 314);
+            this.comboRol.Name = "comboRol";
+            this.comboRol.Size = new System.Drawing.Size(162, 40);
+            this.comboRol.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 32);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Cambiar rol";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(613, 314);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(152, 78);
+            this.btnGuardar.TabIndex = 23;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // AdminVistaUsuarioDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 415);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboRol);
+            this.Controls.Add(this.panel1);
+            this.Name = "AdminVistaUsuarioDetalle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AdminVistaUsuarioDetalle";
+            this.Load += new System.EventHandler(this.AdminVistaUsuarioDetalle_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblUsuarioCategoria;
+        private System.Windows.Forms.Label lblFechaCategoria;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblEmailCategoria;
+        private System.Windows.Forms.Label lblRolCategoria;
+        private System.Windows.Forms.Label lblRol;
+        private System.Windows.Forms.ComboBox comboRol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/AdminVistaUsuarioDetalle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin
+{
+    public partial class AdminVistaUsuarioDetalle : Form
+    {
+        public AdminVistaUsuarioDetalle()
+        {
+            InitializeComponent();
+        }
+
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string Email { get; set; }
+        public string Rol { get; set; }
+
+        private void AdminVistaUsuarioDetalle_Load(object sender, EventArgs e)
+        {
+            CargarRoles();
+
+            lblUsuario.Text = PrimerNombre + " " + SegundoNombre + " (" + Usuario + ")";
+            lblEmail.Text = Email;
+            lblRol.Text = Rol;
+        }
+
+        private void CargarRoles()
+        {
+            comboRol.Items.Add("Cliente");
+            comboRol.Items.Add("Proveedor");
+            comboRol.Items.Add("Admin");
+        }
+    }
+}
