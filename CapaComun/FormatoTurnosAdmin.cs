981389ce5977d687ef057931b9c5f18ffb7263ac
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 09:00:01 -0300

    Agregado formato de turnos para el admin

---
+++ CapaComun/FormatoTurnosAdmin.cs
@@ -0,0 +1,18 @@
+﻿namespace CapaComun
+{
+    public class FormatoTurnosAdmin
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Mes { get; set; }
+        public string Anio { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string Usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+    }
+}
