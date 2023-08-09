9a5e949d9705371c2864b643cd23a136f8abb62d
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 07:20:45 -0300

    Mostrar usuarios

---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/Componentes/UsuarioItem.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin.Componentes
+{
+    partial class UsuarioItem
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblRol = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(32, 37);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(112, 32);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(234, 37);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(114, 32);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblRol
+            // 
+            this.lblRol.AutoSize = true;
+            this.lblRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRol.Location = new System.Drawing.Point(643, 37);
+            this.lblRol.Name = "lblRol";
+            this.lblRol.Size = new System.Drawing.Size(57, 32);
+            this.lblRol.TabIndex = 4;
+            this.lblRol.Text = "Rol";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(434, 37);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(118, 32);
+            this.lblApellido.TabIndex = 3;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(811, 37);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(86, 32);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email";
+            // 
+            // UsuarioItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblRol);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "UsuarioItem";
+            this.Size = new System.Drawing.Size(1102, 123);
+            this.Load += new System.EventHandler(this.UsuarioItem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblRol;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/Componentes/UsuarioItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin.Componentes
+{
+    public partial class UsuarioItem : UserControl
+    {
+        public UsuarioItem()
+        {
+            InitializeComponent();
+        }
+
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string Email { get; set; }
+        public string Rol { get; set; }
+
+        private void UsuarioItem_Load(object sender, System.EventArgs e)
+        {
+            lblUsuario.Text = Usuario;
+            lblNombre.Text = PrimerNombre;
+            lblApellido.Text = SegundoNombre;
+            lblEmail.Text = Email;
+            lblRol.Text = Rol;
+        }
+    }
+}
