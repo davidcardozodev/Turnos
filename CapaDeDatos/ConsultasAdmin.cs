0eddcc211f18984249f999986ea3ff49b0e2834d
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 05:48:31 -0300

    Cargar turnos de los clientes para el admin

---
+++ CapaDeDatos/ConsultasAdmin.cs
@@ -0,0 +1,60 @@
+﻿using CapaComun;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeDatos
+{
+    public class ConsultasAdmin
+    {
+        #region "Objetos"
+
+        private Conexion _Conexion = new Conexion();
+        private SqlDataReader _Lector;
+        private DataTable _Tabla = new DataTable();
+        private SqlCommand _Comando = new SqlCommand();
+        private List<FormatoTurnos> _Valores = new List<FormatoTurnos>();
+
+        #endregion
+
+        public List<FormatoTurnos> TurnoCargarAdmin()
+        {
+            _Comando.Connection = _Conexion.ConexionAbrir();
+            _Comando.CommandText = "TurnoCargarAdmin";
+            _Comando.CommandType = CommandType.StoredProcedure;
+
+            _Lector = _Comando.ExecuteReader();
+
+            while (_Lector.Read())
+            {
+                int Id = int.Parse(_Lector["Id"].ToString());
+                int IdCliente = int.Parse(_Lector["IdCliente"].ToString());
+                string DiaNombre = _Lector["DiaNombre"].ToString();
+                string DiaNumero = _Lector["DiaNumero"].ToString();
+                string Mes = _Lector["Mes"].ToString();
+                string Anio = _Lector["Anio"].ToString();
+                string Hora = _Lector["Hora"].ToString();
+                string Descripcion = _Lector["Descripcion"].ToString();
+                string Estado = _Lector["Estado"].ToString();
+
+                _Valores.Add(new FormatoTurnos
+                {
+                    Id = Id,
+                    IdCliente = IdCliente,
+                    DiaNombre = DiaNombre,
+                    DiaNumero = DiaNumero,
+                    Mes = Mes,
+                    Anio = Anio,
+                    Hora = Hora,
+                    Descripcion = Descripcion,
+                    Estado = Estado,
+                });
+            }
+
+            _Conexion.ConexionCerrar();
+
+            return _Valores;
+        }
+
+    }
+}
