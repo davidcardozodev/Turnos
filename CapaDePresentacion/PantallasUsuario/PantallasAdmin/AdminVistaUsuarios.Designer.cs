15a4cb971d4764b069275076aa56cc04d1655375
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 07:18:49 -0300

    Pasar datos de los usuarios

---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/AdminVistaUsuarios.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin
+{
+    partial class AdminVistaUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(45, 130);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1176, 518);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(465, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 37);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lista de usuarios";
+            // 
+            // AdminVistaUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1293, 683);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminVistaUsuarios";
+            this.Text = "AdminVistaUsuarios";
+            this.Load += new System.EventHandler(this.AdminVistaUsuarios_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallasAdmin/AdminVistaUsuarios.cs
@@ -0,0 +1,44 @@
+﻿using CapaComun;
+using CapaDeEntidades;
+using CapaDePresentacion.PantallasUsuario.PantallasAdmin.Componentes;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallasAdmin
+{
+    public partial class AdminVistaUsuarios : Form
+    {
+        public AdminVistaUsuarios()
+        {
+            InitializeComponent();
+        }
+        private void AdminVistaUsuarios_Load(object sender, System.EventArgs e)
+        {
+            AdminCargarUsuario();
+        }
+
+        private void AdminCargarUsuario()
+        {
+            Admin admin = new Admin();
+
+            List<DatosUsuarioItem> Usuarios = new List<DatosUsuarioItem>();
+
+            Usuarios = admin.AdminCargarUsuario();
+
+            foreach (DatosUsuarioItem Usuario in Usuarios)
+            {
+                UsuarioItem usuarioItem = new UsuarioItem();
+
+                usuarioItem.Id = Usuario.Id;
+                usuarioItem.Usuario = Usuario.Usuario;
+                usuarioItem.Clave = Usuario.Clave;
+                usuarioItem.PrimerNombre = Usuario.PrimerNombre;
+                usuarioItem.SegundoNombre = Usuario.SegundoNombre;
+                usuarioItem.Email = Usuario.Email;
+                usuarioItem.Rol = Usuario.Rol;
+
+                flowLayoutPanel1.Controls.Add(usuarioItem);
+            }
+        }
+    }
+}
