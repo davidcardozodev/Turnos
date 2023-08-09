a6021b5b4560157884112694cc6039ac1a6678bc
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 02:47:54 -0300

    Agregado vista detalle del turno

--- CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnoDetalle.cs
+++ CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnoDetalle.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace CapaDePresentacion.PantallasUsuario.PantallasCliente
 {
@@ -15,6 +7,27 @@ namespace CapaDePresentacion.PantallasUsuario.PantallasCliente
         public ClienteTurnoDetalle()
         {
             InitializeComponent();
+        }
+
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Mes { get; set; }
+        public string Anio { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+
+        private void ClienteTurnoDetalle_Load(object sender, System.EventArgs e)
+        {
+            CargarInformacion();
+        }
+
+        private void CargarInformacion()
+        {
+            lblFecha.Text = DiaNombre + " " + DiaNumero + ", " + Mes + ", " + Anio;
+            lblHora.Text = Hora;
+            rtxtDecripcion.Text = Descripcion;
+            lblEstado.Text = Estado;
         }
     }
 }
