ae5fb40bcfa0a25ecddb4abe01d33df32f96f5ec
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 01:35:56 -0300

    Agregado icono de opciones a la vista de los turnos

--- CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnosVista.Designer.cs
+++ CapaDePresentacion/PantallasUsuario/PantallasCliente/ClienteTurnosVista.Designer.cs
@@ -48,7 +48,7 @@
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(231, 117);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(786, 518);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(959, 518);
             this.flowLayoutPanel1.TabIndex = 1;
             // 
             // btnVolverAtras
--- CapaDePresentacion/PantallasUsuario/PantallasCliente/Componentes/ClienteTurnoItem.Designer.cs
+++ CapaDePresentacion/PantallasUsuario/PantallasCliente/Componentes/ClienteTurnoItem.Designer.cs
@@ -36,9 +36,11 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panelEstado = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panelEstado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDiaNombre
@@ -104,12 +106,13 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Controls.Add(this.panelEstado);
             this.panel2.Controls.Add(this.lblHora);
             this.panel2.Controls.Add(this.lblDescripcion);
             this.panel2.Location = new System.Drawing.Point(238, 19);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(519, 140);
+            this.panel2.Size = new System.Drawing.Size(635, 140);
             this.panel2.TabIndex = 6;
             // 
             // panelEstado
@@ -121,6 +124,16 @@
             this.panelEstado.Size = new System.Drawing.Size(295, 51);
             this.panelEstado.TabIndex = 5;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaDePresentacion.Properties.Resources.tresPuntos;
+            this.pictureBox1.Location = new System.Drawing.Point(550, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
             // ClienteTurnoItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -129,7 +142,7 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "ClienteTurnoItem";
-            this.Size = new System.Drawing.Size(776, 178);
+            this.Size = new System.Drawing.Size(929, 178);
             this.Load += new System.EventHandler(this.ClienteTurnoItem_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -137,6 +150,7 @@
             this.panel2.PerformLayout();
             this.panelEstado.ResumeLayout(false);
             this.panelEstado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -151,5 +165,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panelEstado;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
