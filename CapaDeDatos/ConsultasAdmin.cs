123c210eb5e2c6e3e227ce17c2f5858818da3d8f
David Cardozo <davidcardozoemma@gmail.com>
2023-08-09 07:15:35 -0300

    Cargar datos de los usuarios

--- CapaDeDatos/ConsultasAdmin.cs
+++ CapaDeDatos/ConsultasAdmin.cs
@@ -14,8 +14,44 @@ namespace CapaDeDatos
         private DataTable _Tabla = new DataTable();
         private SqlCommand _Comando = new SqlCommand();
         private List<FormatoTurnos> _Valores = new List<FormatoTurnos>();
+        private List<DatosUsuarioItem> _Usuarios = new List<DatosUsuarioItem>();
 
         #endregion
+
+        public List<DatosUsuarioItem> AdminCargarUsuario()
+        {
+            _Comando.Connection = _Conexion.ConexionAbrir();
+            _Comando.CommandText = "AdminCargarUsuario";
+            _Comando.CommandType = CommandType.StoredProcedure;
+
+            _Lector = _Comando.ExecuteReader();
+
+            while (_Lector.Read())
+            {
+                int Id = int.Parse(_Lector["Id"].ToString());
+                string Usuario = _Lector["Usuario"].ToString();
+                string Clave = _Lector["Clave"].ToString();
+                string PrimerNombre = _Lector["PrimerNombre"].ToString();
+                string SegundoNombre = _Lector["SegundoNombre"].ToString();
+                string Email = _Lector["Email"].ToString();
+                string Rol = _Lector["Rol"].ToString();
+
+                _Usuarios.Add(new DatosUsuarioItem
+                {
+                    Id = Id,
+                    Usuario = Usuario,
+                    Clave = Clave,
+                    PrimerNombre = PrimerNombre,
+                    SegundoNombre = SegundoNombre,
+                    Email = Email,
+                    Rol = Rol
+                });
+            }
+
+            _Conexion.ConexionCerrar();
+
+            return _Usuarios;
+        }
 
         public List<FormatoTurnos> TurnoCargarAdmin()
         {
