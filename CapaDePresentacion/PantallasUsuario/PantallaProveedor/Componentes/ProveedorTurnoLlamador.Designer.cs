229feb541b613e2f20451ac2327be45e33dc7f3f
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 11:39:40 -0300

    Agregado turno del llamador

---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/Componentes/ProveedorTurnoLlamador.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor.Componentes
+{
+    partial class ProveedorTurnoLlamador
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightCyan;
+            this.panel2.Controls.Add(this.btnAbrir);
+            this.panel2.Controls.Add(this.lblCliente);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(446, 127);
+            this.panel2.TabIndex = 12;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(98, 45);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(104, 32);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrir.Location = new System.Drawing.Point(17, 35);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(64, 52);
+            this.btnAbrir.TabIndex = 6;
+            this.btnAbrir.Text = "+";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            // 
+            // ProveedorTurnoLlamador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Name = "ProveedorTurnoLlamador";
+            this.Size = new System.Drawing.Size(446, 127);
+            this.Load += new System.EventHandler(this.ProveedorTurnoLlamador_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
