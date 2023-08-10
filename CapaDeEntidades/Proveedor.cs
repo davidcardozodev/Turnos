173e861ccf2f5c41afde0be189d9f5274ab3ea38
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 05:05:14 -0300

    Agregado entidad de proveedor

---
+++ CapaDeEntidades/Proveedor.cs
@@ -0,0 +1,18 @@
+﻿using CapaComun;
+using CapaDeDatos;
+using System.Collections.Generic;
+
+namespace CapaDeEntidades
+{
+    public class Proveedor
+    {
+        public Proveedor() { }
+
+        ConsultasProveedor _ConsultasProveedor = new ConsultasProveedor();
+
+        public List<FormatoTurnosAdmin> TurnoCargarProveedor()
+        {
+            return _ConsultasProveedor.TurnoCargarProveedor();
+        }
+    }
+}
