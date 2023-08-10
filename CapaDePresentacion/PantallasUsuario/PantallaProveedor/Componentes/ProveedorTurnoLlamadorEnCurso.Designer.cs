527e822eddae10999143720e4687dee56ce1d29f
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 12:13:24 -0300

    Agregado turno en curso

---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/Componentes/ProveedorTurnoLlamadorEnCurso.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor.Componentes
+{
+    partial class ProveedorTurnoLlamadorEnCurso
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(18, 41);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(104, 32);
+            this.lblCliente.TabIndex = 6;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // ProveedorTurnoLlamadorEnCurso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Controls.Add(this.lblCliente);
+            this.Name = "ProveedorTurnoLlamadorEnCurso";
+            this.Size = new System.Drawing.Size(439, 120);
+            this.Load += new System.EventHandler(this.ProveedorTurnoLlamadorEnCurso_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCliente;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/Componentes/ProveedorTurnoLlamadorEnCurso.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor.Componentes
+{
+    public partial class ProveedorTurnoLlamadorEnCurso : UserControl
+    {
+        public ProveedorTurnoLlamadorEnCurso()
+        {
+            InitializeComponent();
+        }
+
+        private void ProveedorTurnoLlamadorEnCurso_Load(object sender, System.EventArgs e)
+        {
+            CargarInformacion();
+        }
+
+        public int Id { get; set; }
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Mes { get; set; }
+        public string Anio { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string Usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+
+        private void CargarInformacion()
+        {
+            lblCliente.Text = PrimerNombre + " " + SegundoNombre;
+        }
+
+    }
+}
