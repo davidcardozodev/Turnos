a9e38a68826ff19ff022581d20704a1ad62f410c
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 05:32:53 -0300

    Agregado pantalla principal de proveedor

---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorPrincipal.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    partial class ProveedorPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnTurnosClientes = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnTurnosClientes);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnTurnosClientes
+            // 
+            this.btnTurnosClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTurnosClientes.Location = new System.Drawing.Point(218, 163);
+            this.btnTurnosClientes.Name = "btnTurnosClientes";
+            this.btnTurnosClientes.Size = new System.Drawing.Size(310, 111);
+            this.btnTurnosClientes.TabIndex = 2;
+            this.btnTurnosClientes.Text = "Turnos de los clientes";
+            this.btnTurnosClientes.UseVisualStyleBackColor = true;
+            this.btnTurnosClientes.Click += new System.EventHandler(this.btnTurnosClientes_Click);
+            // 
+            // ProveedorPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProveedorPrincipal";
+            this.Text = "ProveedorPrincipal";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTurnosClientes;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorPrincipal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    public partial class ProveedorPrincipal : Form
+    {
+        public ProveedorPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTurnosClientes_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario<ProveedorTurnosVista>();
+        }
+
+        #region "Cargar Formulario"
+
+        public void AbrirFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new Form();
+            Formulario = panel1.Controls.OfType<MiFormulario>().FirstOrDefault();
+            Formulario = CrearFormulario<MiFormulario>(Formulario);
+        }
+
+        public Form CrearFormulario<MiFormulario>(Form Formulario) where MiFormulario : Form, new()
+        {
+            if (Formulario == null)
+                Formulario = CargarFormulario<MiFormulario>();
+            else
+                Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        public Form CargarFormulario<MiFormulario>() where MiFormulario : Form, new()
+        {
+            Form Formulario = new MiFormulario();
+
+            Formulario.TopLevel = false;
+            Formulario.Dock = DockStyle.Fill;
+
+            panel1.Controls.Add(Formulario);
+            panel1.Tag = Formulario;
+
+            Formulario.Show();
+            Formulario.BringToFront();
+
+            return Formulario;
+        }
+
+        #endregion
+    }
+}
