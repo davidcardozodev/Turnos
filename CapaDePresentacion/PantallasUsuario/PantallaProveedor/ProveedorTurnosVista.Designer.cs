838f84469068d2f2326066b3a952221b2aed5b3e
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 05:41:49 -0300

    Agregado vista de turnos de proveedor

---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorTurnosVista.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    partial class ProveedorTurnosVista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVolverAtras = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVolverAtras
+            // 
+            this.btnVolverAtras.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVolverAtras.Location = new System.Drawing.Point(47, 21);
+            this.btnVolverAtras.Name = "btnVolverAtras";
+            this.btnVolverAtras.Size = new System.Drawing.Size(204, 58);
+            this.btnVolverAtras.TabIndex = 7;
+            this.btnVolverAtras.Text = "Volver atras";
+            this.btnVolverAtras.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(261, 114);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(756, 518);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(359, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Turnos agendados de los clientes";
+            // 
+            // ProveedorTurnosVista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 652);
+            this.Controls.Add(this.btnVolverAtras);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ProveedorTurnosVista";
+            this.Text = "ProveedorTurnosVista";
+            this.Load += new System.EventHandler(this.ProveedorTurnosVista_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVolverAtras;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorTurnosVista.cs
@@ -0,0 +1,50 @@
+﻿using CapaComun;
+using CapaDeEntidades;
+using CapaDePresentacion.PantallasUsuario.PantallaProveedor.Componentes;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    public partial class ProveedorTurnosVista : Form
+    {
+        public ProveedorTurnosVista()
+        {
+            InitializeComponent();
+        }
+
+        private void ProveedorTurnosVista_Load(object sender, System.EventArgs e)
+        {
+            CargarTurnoItem();
+        }
+
+        private void CargarTurnoItem()
+        {
+            Proveedor proveedor = new Proveedor();
+
+            List<FormatoTurnosAdmin> ListaTurnos = new List<FormatoTurnosAdmin>();
+
+            ListaTurnos = proveedor.TurnoCargarProveedor();
+
+            foreach (FormatoTurnosAdmin Turno in ListaTurnos)
+            {
+                ProveedorTurnoItem proveedorTurnoItem = new ProveedorTurnoItem();
+
+                proveedorTurnoItem.Id = Turno.Id;
+                proveedorTurnoItem.DiaNombre = Turno.DiaNombre;
+                proveedorTurnoItem.DiaNumero = Turno.DiaNumero;
+                proveedorTurnoItem.Mes = Turno.Mes;
+                proveedorTurnoItem.Anio = Turno.Anio;
+                proveedorTurnoItem.Hora = Turno.Hora;
+                proveedorTurnoItem.Descripcion = Turno.Descripcion;
+                proveedorTurnoItem.Estado = Turno.Estado;
+                proveedorTurnoItem.Usuario = Turno.Usuario;
+                proveedorTurnoItem.PrimerNombre = Turno.PrimerNombre;
+                proveedorTurnoItem.SegundoNombre = Turno.SegundoNombre;
+
+                flowLayoutPanel1.Controls.Add(proveedorTurnoItem);
+            }
+        }
+
+    }
+}
