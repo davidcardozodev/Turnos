da8dd5ab6b8839661e1b7d3b1242021406acd8bf
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 11:27:29 -0300

    Agregado cargar turnos asignados segun proveedor

--- CapaDeDatos/ConsultasProveedor.cs
+++ CapaDeDatos/ConsultasProveedor.cs
@@ -62,6 +62,52 @@ namespace CapaDeDatos
             return _Valores;
         }
 
+        public List<FormatoTurnosAdmin> TurnoCargarProveedorAsignado(int idProveedor)
+        {
+            _Comando.Connection = _Conexion.ConexionAbrir();
+            _Comando.CommandText = "TurnoCargarProveedorAsignado";
+            _Comando.CommandType = CommandType.StoredProcedure;
+            _Comando.Parameters.AddWithValue("idProveedor", idProveedor);
+
+            _Lector = _Comando.ExecuteReader();
+
+            while (_Lector.Read())
+            {
+                int Id = int.Parse(_Lector["Id"].ToString());
+                int IdCliente = int.Parse(_Lector["IdCliente"].ToString());
+                string DiaNombre = _Lector["DiaNombre"].ToString();
+                string DiaNumero = _Lector["DiaNumero"].ToString();
+                string Mes = _Lector["Mes"].ToString();
+                string Anio = _Lector["Anio"].ToString();
+                string Hora = _Lector["Hora"].ToString();
+                string Descripcion = _Lector["Descripcion"].ToString();
+                string Estado = _Lector["Estado"].ToString();
+                string Usuario = _Lector["Usuario"].ToString();
+                string PrimerNombre = _Lector["PrimerNombre"].ToString();
+                string SegundoNombre = _Lector["SegundoNombre"].ToString();
+
+                _Valores.Add(new FormatoTurnosAdmin
+                {
+                    Id = Id,
+                    IdCliente = IdCliente,
+                    DiaNombre = DiaNombre,
+                    DiaNumero = DiaNumero,
+                    Mes = Mes,
+                    Anio = Anio,
+                    Hora = Hora,
+                    Descripcion = Descripcion,
+                    Estado = Estado,
+                    Usuario = Usuario,
+                    PrimerNombre = PrimerNombre,
+                    SegundoNombre = SegundoNombre
+                });
+            }
+
+            _Conexion.ConexionCerrar();
+
+            return _Valores;
+        }
+
         public void ProveedorAsignar(int idProveedor, int idTurno)
         {
             _Comando.Connection = _Conexion.ConexionAbrir();
--- CapaDeEntidades/Proveedor.cs
+++ CapaDeEntidades/Proveedor.cs
@@ -15,6 +15,11 @@ namespace CapaDeEntidades
             return _ConsultasProveedor.TurnoCargarProveedor();
         }
 
+        public List<FormatoTurnosAdmin> TurnoCargarProveedorAsignado(int idProveedor)
+        {
+            return _ConsultasProveedor.TurnoCargarProveedorAsignado(idProveedor);
+        }
+
         public void ProveedorAsignar(int idProveedor, int idTurno)
         {
             _ConsultasProveedor.ProveedorAsignar(idProveedor, idTurno);
