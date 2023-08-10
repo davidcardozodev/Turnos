be0d8fbab5776584ee3776857ecb966a4f10f820
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 12:11:42 -0300

    Guardar y cargar turno en curso

--- CapaDeEntidades/Proveedor.cs
+++ CapaDeEntidades/Proveedor.cs
@@ -20,9 +20,19 @@ namespace CapaDeEntidades
             return _ConsultasProveedor.TurnoCargarProveedorAsignado(idProveedor);
         }
 
+        public List<FormatoTurnosAdmin> TurnoCargarProveedorEnCurso(int idProveedor)
+        {
+            return _ConsultasProveedor.TurnoCargarProveedorEnCurso(idProveedor);
+        }
+
         public void ProveedorAsignar(int idProveedor, int idTurno)
         {
             _ConsultasProveedor.ProveedorAsignar(idProveedor, idTurno);
+        }
+
+        public void ProveedorAgregarEnCurso(int idTurno)
+        {
+            _ConsultasProveedor.ProveedorAgregarEnCurso(idTurno);
         }
     }
 }
