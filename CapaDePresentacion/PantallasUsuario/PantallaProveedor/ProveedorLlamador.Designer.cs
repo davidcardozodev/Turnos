577a4059e8339cc67fd2ee3640bf7be74494dd84
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 12:22:51 -0300

    Agregado etiquetas de turnos

--- CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorLlamador.Designer.cs
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorLlamador.Designer.cs
@@ -32,6 +32,8 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblTurnosPendientes = new System.Windows.Forms.Label();
+            this.lblTurnosCurso = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnVolverAtras
@@ -48,7 +50,7 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 105);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 199);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(630, 518);
             this.flowLayoutPanel1.TabIndex = 9;
@@ -66,16 +68,38 @@
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.AutoScroll = true;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(704, 105);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(704, 199);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(630, 518);
             this.flowLayoutPanel2.TabIndex = 10;
+            // 
+            // lblTurnosPendientes
+            // 
+            this.lblTurnosPendientes.AutoSize = true;
+            this.lblTurnosPendientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnosPendientes.Location = new System.Drawing.Point(16, 147);
+            this.lblTurnosPendientes.Name = "lblTurnosPendientes";
+            this.lblTurnosPendientes.Size = new System.Drawing.Size(282, 37);
+            this.lblTurnosPendientes.TabIndex = 11;
+            this.lblTurnosPendientes.Text = "Turnos pendientes";
+            // 
+            // lblTurnosCurso
+            // 
+            this.lblTurnosCurso.AutoSize = true;
+            this.lblTurnosCurso.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnosCurso.Location = new System.Drawing.Point(697, 137);
+            this.lblTurnosCurso.Name = "lblTurnosCurso";
+            this.lblTurnosCurso.Size = new System.Drawing.Size(250, 37);
+            this.lblTurnosCurso.TabIndex = 12;
+            this.lblTurnosCurso.Text = "Turnos en curso";
             // 
             // ProveedorLlamador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1366, 645);
+            this.ClientSize = new System.Drawing.Size(1366, 719);
+            this.Controls.Add(this.lblTurnosCurso);
+            this.Controls.Add(this.lblTurnosPendientes);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.btnVolverAtras);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -95,5 +119,7 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label lblTurnosPendientes;
+        private System.Windows.Forms.Label lblTurnosCurso;
     }
 }
