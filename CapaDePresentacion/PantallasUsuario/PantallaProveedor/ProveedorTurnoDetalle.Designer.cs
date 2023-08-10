fd2da9677f1b93beec83c082b7c3d522ff3f52bb
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 10:56:04 -0300

    Agregado detalle de turno de proveedor

---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorTurnoDetalle.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    partial class ProveedorTurnoDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblUsuarioCategoria = new System.Windows.Forms.Label();
+            this.rtxtDecripcion = new System.Windows.Forms.RichTextBox();
+            this.lblFechaCategoria = new System.Windows.Forms.Label();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.lblEstadoCategoria = new System.Windows.Forms.Label();
+            this.lblDescripcionCategoria = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblHoraCategoria = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.btnAsignar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblUsuario);
+            this.panel1.Controls.Add(this.lblUsuarioCategoria);
+            this.panel1.Controls.Add(this.rtxtDecripcion);
+            this.panel1.Controls.Add(this.lblFechaCategoria);
+            this.panel1.Controls.Add(this.lblEstado);
+            this.panel1.Controls.Add(this.lblEstadoCategoria);
+            this.panel1.Controls.Add(this.lblDescripcionCategoria);
+            this.panel1.Controls.Add(this.lblHora);
+            this.panel1.Controls.Add(this.lblHoraCategoria);
+            this.panel1.Controls.Add(this.lblFecha);
+            this.panel1.Location = new System.Drawing.Point(23, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(730, 396);
+            this.panel1.TabIndex = 20;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.DimGray;
+            this.lblUsuario.Location = new System.Drawing.Point(206, 94);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(112, 32);
+            this.lblUsuario.TabIndex = 16;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblUsuarioCategoria
+            // 
+            this.lblUsuarioCategoria.AutoSize = true;
+            this.lblUsuarioCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuarioCategoria.Location = new System.Drawing.Point(75, 94);
+            this.lblUsuarioCategoria.Name = "lblUsuarioCategoria";
+            this.lblUsuarioCategoria.Size = new System.Drawing.Size(112, 32);
+            this.lblUsuarioCategoria.TabIndex = 15;
+            this.lblUsuarioCategoria.Text = "Usuario";
+            // 
+            // rtxtDecripcion
+            // 
+            this.rtxtDecripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtDecripcion.Location = new System.Drawing.Point(209, 230);
+            this.rtxtDecripcion.Name = "rtxtDecripcion";
+            this.rtxtDecripcion.ReadOnly = true;
+            this.rtxtDecripcion.Size = new System.Drawing.Size(492, 91);
+            this.rtxtDecripcion.TabIndex = 14;
+            this.rtxtDecripcion.Text = "";
+            // 
+            // lblFechaCategoria
+            // 
+            this.lblFechaCategoria.AutoSize = true;
+            this.lblFechaCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaCategoria.Location = new System.Drawing.Point(75, 24);
+            this.lblFechaCategoria.Name = "lblFechaCategoria";
+            this.lblFechaCategoria.Size = new System.Drawing.Size(93, 32);
+            this.lblFechaCategoria.TabIndex = 5;
+            this.lblFechaCategoria.Text = "Fecha";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.ForeColor = System.Drawing.Color.DimGray;
+            this.lblEstado.Location = new System.Drawing.Point(206, 340);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(103, 32);
+            this.lblEstado.TabIndex = 13;
+            this.lblEstado.Text = "Estado";
+            // 
+            // lblEstadoCategoria
+            // 
+            this.lblEstadoCategoria.AutoSize = true;
+            this.lblEstadoCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstadoCategoria.Location = new System.Drawing.Point(65, 340);
+            this.lblEstadoCategoria.Name = "lblEstadoCategoria";
+            this.lblEstadoCategoria.Size = new System.Drawing.Size(103, 32);
+            this.lblEstadoCategoria.TabIndex = 9;
+            this.lblEstadoCategoria.Text = "Estado";
+            // 
+            // lblDescripcionCategoria
+            // 
+            this.lblDescripcionCategoria.AutoSize = true;
+            this.lblDescripcionCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcionCategoria.Location = new System.Drawing.Point(5, 242);
+            this.lblDescripcionCategoria.Name = "lblDescripcionCategoria";
+            this.lblDescripcionCategoria.Size = new System.Drawing.Size(163, 32);
+            this.lblDescripcionCategoria.TabIndex = 8;
+            this.lblDescripcionCategoria.Text = "Descripcion";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.ForeColor = System.Drawing.Color.DimGray;
+            this.lblHora.Location = new System.Drawing.Point(206, 172);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(75, 32);
+            this.lblHora.TabIndex = 11;
+            this.lblHora.Text = "Hora";
+            // 
+            // lblHoraCategoria
+            // 
+            this.lblHoraCategoria.AutoSize = true;
+            this.lblHoraCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraCategoria.Location = new System.Drawing.Point(93, 172);
+            this.lblHoraCategoria.Name = "lblHoraCategoria";
+            this.lblHoraCategoria.Size = new System.Drawing.Size(75, 32);
+            this.lblHoraCategoria.TabIndex = 7;
+            this.lblHoraCategoria.Text = "Hora";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFecha.Location = new System.Drawing.Point(206, 24);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(93, 32);
+            this.lblFecha.TabIndex = 10;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // btnAsignar
+            // 
+            this.btnAsignar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsignar.Location = new System.Drawing.Point(549, 492);
+            this.btnAsignar.Name = "btnAsignar";
+            this.btnAsignar.Size = new System.Drawing.Size(204, 58);
+            this.btnAsignar.TabIndex = 22;
+            this.btnAsignar.Text = "Asignar";
+            this.btnAsignar.UseVisualStyleBackColor = true;
+            // 
+            // ProveedorTurnoDetalle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 562);
+            this.Controls.Add(this.btnAsignar);
+            this.Controls.Add(this.panel1);
+            this.Name = "ProveedorTurnoDetalle";
+            this.Text = "ProveedorTurnoDetalle";
+            this.Load += new System.EventHandler(this.ProveedorTurnoDetalle_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblUsuarioCategoria;
+        private System.Windows.Forms.RichTextBox rtxtDecripcion;
+        private System.Windows.Forms.Label lblFechaCategoria;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Label lblEstadoCategoria;
+        private System.Windows.Forms.Label lblDescripcionCategoria;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblHoraCategoria;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Button btnAsignar;
+    }
+}
---
+++ CapaDePresentacion/PantallasUsuario/PantallaProveedor/ProveedorTurnoDetalle.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Forms;
+
+namespace CapaDePresentacion.PantallasUsuario.PantallaProveedor
+{
+    public partial class ProveedorTurnoDetalle : Form
+    {
+        public ProveedorTurnoDetalle()
+        {
+            InitializeComponent();
+        }
+
+        private void ProveedorTurnoDetalle_Load(object sender, System.EventArgs e)
+        {
+            CargarInformacion();
+        }
+
+        public int Id { get; set; }
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Mes { get; set; }
+        public string Anio { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string Usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+
+        private void CargarInformacion()
+        {
+            lblFecha.Text = DiaNombre + " " + DiaNumero + ", " + Mes + ", " + Anio;
+            lblUsuario.Text = PrimerNombre + " " + SegundoNombre + " (" + Usuario + ")";
+            lblHora.Text = Hora;
+            rtxtDecripcion.Text = Descripcion;
+            lblEstado.Text = Estado;
+        }
+
+
+    }
+}
