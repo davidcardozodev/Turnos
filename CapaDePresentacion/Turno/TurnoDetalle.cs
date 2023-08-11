e3e51255c2a03a6f0db020bd04c57873d721ae02
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 23:38:11 -0300

    Agregado detalle de turno generico

---
+++ CapaDePresentacion/Turno/TurnoDetalle.cs
@@ -0,0 +1,79 @@
+﻿using CapaComun;
+using CapaDeEntidades;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CapaDePresentacion.Turno
+{
+    public partial class TurnoDetalle : Form
+    {
+        public TurnoDetalle()
+        {
+            InitializeComponent();
+        }
+
+        private void TurnoDetalle_Load(object sender, EventArgs e)
+        {
+            CargarInformacion();
+            CambiarColorEstado();
+        }
+        public int Id { get; set; }
+        public string DiaNombre { get; set; }
+        public string DiaNumero { get; set; }
+        public string Mes { get; set; }
+        public string Anio { get; set; }
+        public string Hora { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string Usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string NombreProveedor { get; set; }
+
+
+        private void CargarInformacion()
+        {
+            lblFecha.Text = DiaNombre + " " + DiaNumero + ", " + Mes + ", " + Anio;
+            lblUsuario.Text = PrimerNombre + " " + SegundoNombre + " (" + Usuario + ")";
+            lblHora.Text = Hora + ":00hs";
+            rtxtDecripcion.Text = Descripcion;
+            lblEstado.Text = Estado;
+            lblProveedor.Text = NombreProveedor;
+        }
+
+        private void btnDarDeBaja_Click(object sender, System.EventArgs e)
+        {
+            Admin admin = new Admin();
+
+            admin.TurnoDarBaja(Id);
+        }
+
+        private void CambiarColorEstado()
+        {
+            switch (Estado)
+            {
+                case "Cancelado":
+                    lblEstado.ForeColor = Color.FromArgb(255, 0, 0);
+                    break;
+                case "Finalizado":
+                    lblEstado.ForeColor = Color.FromArgb(0, 255, 0);
+                    break;
+                case "Pendiente":
+                case "Asignado":
+                case "EnCurso":
+                    lblEstado.ForeColor = Color.FromArgb(0, 0, 255);
+                    break;
+            }
+        }
+
+        private void btnAsignar_Click(object sender, EventArgs e)
+        {
+            Proveedor proveedor = new Proveedor();
+
+            proveedor.ProveedorAsignar(DatosUsuario.Id, Id);
+
+            MessageBox.Show("Cambios guardados");
+        }
+    }
+}
