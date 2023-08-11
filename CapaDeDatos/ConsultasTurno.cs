0c776d1757893ec0e2e40076d26675d2e6375e07
David Cardozo <davidcardozoemma@gmail.com>
2023-08-10 23:54:55 -0300

    Agregado entidad turno

---
+++ CapaDeDatos/ConsultasTurno.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDeDatos
+{
+    public class ConsultasTurno
+    {
+        #region "Objetos"
+
+        private Conexion _Conexion = new Conexion();
+        private SqlDataReader _Lector;
+        private DataTable _Tabla = new DataTable();
+        private SqlCommand _Comando = new SqlCommand();
+
+        #endregion
+
+        public void TurnoDarBaja(int idTurno)
+        {
+            _Comando.Connection = _Conexion.ConexionAbrir();
+            _Comando.CommandText = "TurnoDarBaja";
+            _Comando.CommandType = CommandType.StoredProcedure;
+            _Comando.Parameters.AddWithValue("@idTurno", idTurno);
+            _Comando.ExecuteNonQuery();
+            _Conexion.ConexionCerrar();
+        }
+    }
+}
---
+++ CapaDeEntidades/Turno.cs
@@ -0,0 +1,16 @@
+﻿using CapaDeDatos;
+
+namespace CapaDeEntidades
+{
+    public class Turno
+    {
+        public Turno() { }
+
+        private ConsultasTurno _ConsultasTurno = new ConsultasTurno();
+
+        public void TurnoDarBaja(int idTurno)
+        {
+            _ConsultasTurno.TurnoDarBaja(idTurno);
+        }
+    }
+}
