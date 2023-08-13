6ae2db6d6ccf45855bdfae86b6ab45812230b510
David Cardozo <davidcardozoemma@gmail.com>
2023-08-12 23:09:52 -0300

    Agregado clase estados

---
+++ CapaDeNegocio/Estados.cs
@@ -0,0 +1,12 @@
+﻿namespace CapaDeNegocio
+{
+    public class Estados
+    {
+        public const string SinDefinir = "Sin definir";
+        public const string Cancelado = "Cancelado";
+        public const string Finalizado = "Finalizado";
+        public const string Pendiente = "Pendiente";
+        public const string Asignado = "Asignado";
+        public const string EnCurso = "En curso";
+    }
+}
