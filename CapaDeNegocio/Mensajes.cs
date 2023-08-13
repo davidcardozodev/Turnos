f39a9babe86d76e2a611be5657ab2c3de8b4fbed
David Cardozo <davidcardozoemma@gmail.com>
2023-08-12 22:42:52 -0300

    Agregado clase de mensajes

---
+++ CapaDeNegocio/Mensajes.cs
@@ -0,0 +1,9 @@
+﻿namespace CapaDeNegocio
+{
+    public class Mensajes
+    {
+        public const string GuardadoCambios = "Cambios guardados";
+        public const string GuardadoTurno = "Turno guardado";
+        public const string ErrorDB = "Error: No se encontro la base de datos o el procedimiento almacenado";
+    }
+}
