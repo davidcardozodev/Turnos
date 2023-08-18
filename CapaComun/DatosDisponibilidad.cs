80a76b4515888545957091e487235d35a4b45429
David Cardozo <davidcardozoemma@gmail.com>
2023-08-18 15:50:11 -0300

    Agregado: datos de disponibilidad del proveedor

---
+++ CapaComun/DatosDisponibilidad.cs
@@ -0,0 +1,8 @@
+﻿namespace CapaComun
+{
+    public class DatosDisponibilidad
+    {
+        public static int HorarioInicio { get; set; }
+        public static int HorarioFin { get; set; }
+    }
+}
