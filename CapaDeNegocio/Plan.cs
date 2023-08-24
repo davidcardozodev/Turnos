781106443c81a28db677db5d603ada4645ec3e6a
David Cardozo <davidcardozoemma@gmail.com>
2023-08-24 09:01:43 -0300

    Agregado: tipo de plan

---
+++ CapaDeNegocio/Plan.cs
@@ -0,0 +1,9 @@
+﻿namespace CapaDeNegocio
+{
+    public class Plan
+    {
+        public const string Basico = "Basico";
+        public const string Estandar = "Estandar";
+        public const string Premium = "Premium";
+    }
+}
