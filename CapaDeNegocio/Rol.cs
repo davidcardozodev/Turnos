da4ed87ee7464fb0aaffa88bf0f4abf7e130259d
David Cardozo <davidcardozoemma@gmail.com>
2023-08-24 09:28:10 -0300

    Cambiado: nombre de la clase de roles

---
+++ CapaDeNegocio/Rol.cs
@@ -0,0 +1,10 @@
+﻿namespace CapaDeNegocio
+{
+    public class Rol
+    {
+        public const string Cliente = "Cliente";
+        public const string Admin = "Admin";
+        public const string Proveedor = "Proveedor";
+        public const string Llamador = "Llamador";
+    }
+}
