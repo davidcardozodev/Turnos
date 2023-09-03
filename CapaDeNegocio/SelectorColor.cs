10a96135a3576ec895ed8d8a5efecef3e14457b8
David Cardozo <davidcardozoemma@gmail.com>
2023-09-03 00:54:22 -0300

    Clase de seleccion de colores agregado

---
+++ CapaDeNegocio/SelectorColor.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace CapaDeNegocio
+{
+    public class SelectorColor
+    {
+        public Color CambiarColor()
+        {
+            return Color.FromArgb(214, 248, 248);
+        }
+
+        public Color RestaurarColor()
+        {
+            return Color.FromArgb(255, 255, 255);
+        }
+
+        public Color TurnoAsignado()
+        {
+            return Color.FromArgb(16, 196, 192);
+        }
+
+        public Color RestaurarColorTurnoAsignado()
+        {
+            return Color.FromArgb(180, 250, 243);
+        }
+
+        public Color TurnoEnCurso()
+        {
+            return Color.FromArgb(11, 133, 16);
+        }
+
+        public Color RestaurarColorTurnoEnCurso()
+        {
+            return Color.FromArgb(192, 255, 192);
+        }
+    }
+}
