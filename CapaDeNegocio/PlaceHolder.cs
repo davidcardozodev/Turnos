817c8337e95e7df59803a9d54bc42bc0182dfdcb
David Cardozo <davidcardozoemma@gmail.com>
2023-09-08 18:41:54 -0300

    Valores de la opcion del boton de crear calendario con turno agregado

--- CapaDeNegocio/PlaceHolder.cs
+++ CapaDeNegocio/PlaceHolder.cs
@@ -7,5 +7,7 @@
         public const string SeleccionarProveedor = "Seleccione un proveedor";
         public const string SeleccionarLugar = "Seleccione un lugar";
         public const string SeleccionarHorario = "Seleccione un horario";
+        public const string AbrirCreacionTurno = "Abrir turno";
+        public const string CerrarCreacionTurno = "Cerrar turno";
     }
 }
