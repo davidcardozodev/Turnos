f88758321847c6c1b1b3fa87c525d42f3879e52e
David Cardozo <davidcardozoemma@gmail.com>
2023-09-08 18:41:11 -0300

    Mensajes de guardado, error, alerta y aviso agregados

--- CapaDeNegocio/Mensajes.cs
+++ CapaDeNegocio/Mensajes.cs
@@ -4,8 +4,16 @@
     {
         public const string GuardadoCambios = "Cambios guardados";
         public const string GuardadoTurno = "Turno guardado";
+        public const string GuardadoRegistro = "Se registro correctamente";
         public const string ErrorDB = "Error: No se encontro la base de datos o el procedimiento almacenado";
         public const string ErrorArea = "Error: Debe seleccionar un area";
         public const string ErrorEstablecimiento = "Error: Debe seleccionar un establecimiento";
+        public const string ErrorSesion = "Error: Usuario o clave incorrecta";
+        public const string ErrorClave = "Error: Usuario o clave incorrecta";
+        public const string ErrorUsuario = "Error: Usuario o clave incorrecta";
+        public const string ErrorCamposFaltantes = "Error: Todos los campos deben estar llenos";
+        public const string AlertaBajaOpcion = "¿Quiere dar de baja el turno?";
+        public const string AlertaBajaTitulo = "¿Quiere dar de baja el turno?";
+        public const string AdministradorBaja = "Dado de baja por el administrador";
     }
 }
