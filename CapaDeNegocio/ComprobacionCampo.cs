df9875b0ab0ef7dd05fdee0d2d7d900cd83d7f27
David Cardozo <davidcardozoemma@gmail.com>
2023-09-08 18:32:20 -0300

    Comprobacion de campo separado y formatos de fecha agregados

---
+++ CapaDeNegocio/ComprobacionCampo.cs
@@ -0,0 +1,26 @@
+﻿namespace CapaDeNegocio
+{
+    public class ComprobacionCampo
+    {
+        private string Mensaje = Campo.Vacio;
+
+        public string VerificarCamposIndividuales(string campo)
+        {
+            if (campo == Campo.Usuario)
+                Mensaje = Mensajes.ErrorUsuario;
+
+            if (campo == Campo.Clave)
+                Mensaje = Mensajes.ErrorClave;
+
+            return Mensaje;
+        }
+
+        public string VerficarTodosCampos(string CampoUsuario, string CampoClave)
+        {
+            if (CampoUsuario == Campo.Usuario && CampoClave == Campo.Clave)
+                Mensaje = Mensajes.ErrorCamposFaltantes;
+
+            return Mensaje;
+        }
+    }
+}
---
+++ CapaDeNegocio/Fecha.cs
@@ -0,0 +1,10 @@
+﻿namespace CapaDeNegocio
+{
+    public class Fecha
+    {
+        public const string dddd = "dddd";
+        public const string dd = "dd";
+        public const string MMMM = "MMMM";
+        public const string yyyy = "yyyy";
+    }
+}
