8c396f815dd1db8294d8c5872022ca3c22a46e0a
David Cardozo <davidcardozoemma@gmail.com>
2023-09-08 16:44:30 -0300

    Clase de prioridades agregada

---
+++ CapaDeNegocio/Prioridades.cs
@@ -0,0 +1,9 @@
+﻿namespace CapaDeNegocio
+{
+    public class Prioridades
+    {
+        public const string Baja = "Baja";
+        public const string Media = "Media";
+        public const string Alta = "Alta";
+    }
+}
